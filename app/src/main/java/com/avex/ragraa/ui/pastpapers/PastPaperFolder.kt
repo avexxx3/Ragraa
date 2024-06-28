87c37bc605036f365c48d889980363e47f986b07
avex <armaghan_atiq@live.com>
2024-06-29 03:39:53 +0500

    add cascading effect when navigating folders

--- app/src/main/java/com/avex/ragraa/ui/pastpapers/PastPaperFolder.kt
+++ app/src/main/java/com/avex/ragraa/ui/pastpapers/PastPaperFolder.kt
@@ -1,10 +1,11 @@
 package com.avex.ragraa.ui.pastpapers
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -35,68 +36,66 @@ fun PastPaperFolder(
         returnFunction()
     }
 
-    AnimatedVisibility(
-        uiState.showDir,
-        enter = slideInHorizontally(initialOffsetX = { it }),
-        exit = slideOutHorizontally(targetOffsetX = { it }),
-        modifier = Modifier
-    ) {
-        val newViewModel = PastPaperViewModel(uiState.viewingDir!!)
+    AnimatedContent(uiState.showDir, label = "", transitionSpec = {
+        if (uiState.showDir) slideInHorizontally(initialOffsetX = { it }).togetherWith(
+            slideOutHorizontally(targetOffsetX = { -it })
+        )
+        else slideInHorizontally(initialOffsetX = { -it }).togetherWith(
+            slideOutHorizontally(targetOffsetX = { it })
+        )
+    }) { viewingFolder ->
+        if (viewingFolder) {
+            val newViewModel = PastPaperViewModel(uiState.viewingDir!!)
 
-        PastPaperFolder(newViewModel, "$dirName/${uiState.selfDir.name}") {
-            viewModel.hideDir()
-        }
-    }
-
-    AnimatedVisibility(
-        !uiState.showDir,
-        enter = slideInHorizontally(initialOffsetX = { -it }),
-        exit = slideOutHorizontally(targetOffsetX = { -it }),
-        modifier = Modifier
-    ) {
-        LazyColumn(
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            item {
-                if (uiState.selfDir.name.isNotEmpty()) Text(
-                    uiState.selfDir.name,
-                    style = MaterialTheme.typography.headlineMedium,
-                    modifier = Modifier.padding(vertical = 8.dp),
-                    textAlign = TextAlign.Center
-                )
-
-                if (uiState.selfDir.completed) {
-                    for (folder in uiState.selfDir.contents.directories) {
-                        PastPaperFolderCard(folder) { viewModel.setDirectory(folder) }
-                    }
-
-                    for (file in uiState.selfDir.contents.files) {
-                        PastPaperFileCard(
-                            PastPaperFileViewModel(
-                                file, "$dirName/${uiState.selfDir.name}"
-                            )
-                        )
-                    }
-                } else if (uiState.rateLimited) {
-                    Text(
-                        "You are being rate limited.\nPlease try again in around an hour.",
-                        textAlign = TextAlign.Center,
+            PastPaperFolder(newViewModel, "$dirName/${uiState.selfDir.name}") {
+                viewModel.hideDir()
+            }
+        } else {
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                item {
+                    if (uiState.selfDir.name.isNotEmpty()) Text(
+                        uiState.selfDir.name,
                         style = MaterialTheme.typography.headlineMedium,
-                        color = MaterialTheme.colorScheme.error,
-                        modifier = Modifier.padding(32.dp)
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        textAlign = TextAlign.Center
                     )
-                } else {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Spacer(Modifier.weight(1f))
-                        CircularLoadingIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-                        Spacer(Modifier.weight(1f))
-                    }
-                }
 
+                    if (uiState.selfDir.completed) {
+                        for (folder in uiState.selfDir.contents.directories) {
+                            PastPaperFolderCard(folder) { viewModel.setDirectory(folder) }
+                        }
+
+                        for (file in uiState.selfDir.contents.files) {
+                            PastPaperFileCard(
+                                PastPaperFileViewModel(
+                                    file, "$dirName/${uiState.selfDir.name}"
+                                )
+                            )
+                        }
+                    } else if (uiState.rateLimited) {
+                        Text(
+                            "You are being rate limited.\nPlease try again in around an hour.",
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.headlineMedium,
+                            color = MaterialTheme.colorScheme.error,
+                            modifier = Modifier.padding(32.dp)
+                        )
+                    } else {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(Modifier.weight(1f))
+                            CircularLoadingIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+                            Spacer(Modifier.weight(1f))
+                        }
+                    }
+
+                }
             }
         }
     }
+
 }
