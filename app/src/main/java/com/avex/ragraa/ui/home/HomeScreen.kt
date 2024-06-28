55930ca55712c3a941ad9433935b3ad4f35d7f59
avex <armaghan_atiq@live.com>
2024-06-29 02:15:39 +0500

    border from cat has been removed and changing setting makes image fade into another

--- app/src/main/java/com/avex/ragraa/ui/home/HomeScreen.kt
+++ app/src/main/java/com/avex/ragraa/ui/home/HomeScreen.kt
@@ -1,16 +1,12 @@
 package com.avex.ragraa.ui.home
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,9 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -28,10 +21,7 @@ import com.avex.ragraa.R
 import com.avex.ragraa.data.Datasource
 import com.avex.ragraa.ui.Screens
 import com.avex.ragraa.ui.login.Logo
-import com.avex.ragraa.ui.misc.drawRainbowBorder
 
-
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel
@@ -43,31 +33,21 @@ fun HomeScreen(
         viewModel.navController.popBackStack()
     }
     Surface(modifier = Modifier.fillMaxSize()) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Logo(modifier = Modifier.padding(bottom = 4.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(bottom = 8.dp)
+        ) {
+            Logo()
 
-            if (uiState.image != null && uiState.showImage) ProfilePicture() else Box(
-                Modifier.padding(
-                    bottom = 8.dp
-                )
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.cat),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .fillMaxWidth(0.4f)
-                        .aspectRatio(1f)
-                        .drawRainbowBorder(2.dp, 1200, 10000f)
-                        .padding(3.dp)
-                        .clip(CircleShape)
-                )
+            Box(modifier = Modifier.weight(1f)) {
+                ProfilePicture(uiState.showImage)
             }
 
             Text(
                 "${stringResource(R.string.welcome)}\n${Datasource.rollNo},",
                 style = MaterialTheme.typography.displayMedium,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 8.dp)
             )
 
             Text(
--- app/src/main/java/com/avex/ragraa/ui/home/ProfilePicture.kt
+++ app/src/main/java/com/avex/ragraa/ui/home/ProfilePicture.kt
@@ -1,33 +1,60 @@
 package com.avex.ragraa.ui.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.avex.ragraa.R
 import com.avex.ragraa.data.Datasource
 import com.avex.ragraa.ui.misc.drawRainbowBorder
 
 @Composable
-fun ProfilePicture(bitmap: ImageBitmap = Datasource.bitmap!!) {
-    Box(Modifier.padding(bottom = 8.dp)) {
-        Image(
-            bitmap = bitmap,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth(0.4f)
-                .aspectRatio(1f)
-                .drawRainbowBorder(2.dp, 1200, 10000f)
-                .padding(3.dp)
-                .clip(CircleShape)
-        )
+fun ProfilePicture(showImage: Boolean, modifier: Modifier = Modifier) {
+
+    Box {
+        AnimatedVisibility(
+            showImage,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Datasource.bitmap?.let {
+                Image(
+                    bitmap = it,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = modifier
+                        .aspectRatio(1f)
+                        .drawRainbowBorder(2.dp, 1200, 10000f)
+                        .clip(CircleShape)
+                        .fillMaxSize(0.8f)
+                )
+            }
+        }
+
+        AnimatedVisibility(
+            !showImage,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.cat),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight,
+                modifier = modifier
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
+                    .fillMaxSize(0.8f)
+            )
+        }
     }
 }
