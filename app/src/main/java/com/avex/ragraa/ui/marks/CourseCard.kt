9956343d75ef525d6eece32d6aacc64a9130b023
avex <armaghan_atiq@live.com>
2024-06-29 02:15:11 +0500

    background of card has a shimmer if it has new data

--- app/src/main/java/com/avex/ragraa/ui/marks/CourseCard.kt
+++ app/src/main/java/com/avex/ragraa/ui/marks/CourseCard.kt
@@ -1,5 +1,11 @@
 package com.avex.ragraa.ui.marks
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,7 +16,14 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -20,14 +33,56 @@ import com.avex.ragraa.data.Course
 
 @Composable
 fun CourseCard(course: Course, navCourse: () -> Unit, selectCourse: (Course) -> Unit) {
+    val limit = 1f
+
+    val transition = rememberInfiniteTransition(label = "shimmer")
+
+    val progressAnimated by transition.animateFloat(
+        initialValue = -limit,
+        targetValue = limit,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ), label = "shimmer"
+    )
+
+    val primary = MaterialTheme.colorScheme.primaryContainer
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 12.dp)
             .clickable { selectCourse(course); navCourse() }
-        ,
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
-        elevation = CardDefaults.cardElevation(dimensionResource(R.dimen.card_elevation))
+            .drawWithCache {
+                val offset = size.width * progressAnimated
+                val gradientWidth = size.width
+
+                val brush = if (course.newMarks) Brush.linearGradient(
+                    colors = listOf(
+                        primary,
+                        Color(0xFF89FFFD),
+                        Color(0xFFEF32D9),
+                        Color(0xFF89FFFD),
+                        primary
+                    ),
+                    start = Offset(offset, 0f),
+                    end = Offset(offset + gradientWidth, size.height)
+                ) else Brush.linearGradient(listOf(primary, primary))
+
+                onDrawBehind {
+                    drawRoundRect(
+                        brush = brush,
+                        blendMode = BlendMode.SrcIn,
+                        cornerRadius = CornerRadius(12f)
+                    )
+                }
+            },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer.copy(
+                alpha = if (course.newMarks) 0f else 1f
+            )
+        ),
+        elevation = CardDefaults.cardElevation(if (course.newMarks) 0.dp else dimensionResource(R.dimen.card_elevation))
     ) {
 
         val textColor = MaterialTheme.colorScheme.onPrimaryContainer
@@ -38,7 +93,7 @@ fun CourseCard(course: Course, navCourse: () -> Unit, selectCourse: (Course) -> 
             textAlign = TextAlign.Start,
             color = textColor,
             softWrap = true,
-            modifier = Modifier.padding(start = 12.dp, top = 12.dp),
+            modifier = Modifier.padding(start = 12.dp, top = 12.dp, end = 8.dp),
         )
 
         val footerStyle = MaterialTheme.typography.titleMedium
