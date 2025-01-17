dd3a77c20253d4521bb5aa227226c46c8fdae418
avexxx3 <armaghan_atiq@live.com>
2025-01-17 19:20:16 +0500

    randomize the user agents for some reason

--- app/src/main/java/com/avex/ragraa/ui/login/WebViewScreen.kt
+++ app/src/main/java/com/avex/ragraa/ui/login/WebViewScreen.kt
@@ -62,6 +62,18 @@ fun WebViewScreen(
         )
 
         val url = "https://flexstudent.nu.edu.pk/Login"
+        val userAgents = listOf(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.4; rv:124.0) Gecko/20100101 Firefox/124.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0"
+        )
 
         AndroidView(modifier = Modifier
             .fillMaxSize()
@@ -72,8 +84,7 @@ fun WebViewScreen(
                 settings.javaScriptEnabled = true
                 settings.blockNetworkImage = true
                 settings.setSupportZoom(true)
-                settings.userAgentString =
-                    "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.39 Mobile Safari/537.36"
+                settings.userAgentString = userAgents.random()
             }
         }) { webView ->
             if (loading) {
