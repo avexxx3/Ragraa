32c281e19c399d64f2a2cc9d8a98bc139f540dd5
avexxx3 <armaghan_atiq@live.com>
2025-02-22 19:12:08 +0500

    Add indicator when no marks

--- app/src/main/java/com/avex/ragraa/ui/marks/MarksScreen.kt
+++ app/src/main/java/com/avex/ragraa/ui/marks/MarksScreen.kt
@@ -5,10 +5,14 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -39,10 +43,18 @@ fun MarksScreen(
         }
     ) {
         composable("marks") {
-            LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
                 item {
                     if (Datasource.courses.isEmpty()) {
                         Image(painterResource(R.drawable.cat), contentDescription = null)
+                        Text(
+                            "Nothing to show..",
+                            style = MaterialTheme.typography.headlineMedium,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
                     }
                     for (course in Datasource.courses) {
                         CourseCard(course, { newNavController.navigate("course") }) {
